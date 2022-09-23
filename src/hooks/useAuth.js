63c05c66c77e7d8287277af9fb9b88bf53785ff5
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2022-09-23 13:29:50 -0300

    creating auth hook

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,23 @@
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    updateProfile,
+    signOut,
+} from "firebase/auth";
+
+import { useState, useEffect } from "react";
+
+export const useAuth = () => {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  //deal with memory leak
+  const [cancelled, setCancelled] = useState(false);
+  const auth = getAuth();
+  function checkIfIsCancelled() {
+    if(cancelled) {
+        return;
+    }
+  }
+}
